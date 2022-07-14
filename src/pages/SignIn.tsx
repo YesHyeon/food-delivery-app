963fe4c33d8b873d811db6860515d2a9629fbeda
shgus1224 <shgus1224@naver.com>
2022-07-14 22:29:15 +0900

    회원가입 페이지 제작중 및 Redux 구현

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,6 @@
-import React, {useCallback, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, {useCallback, useRef, useState} from 'react';
 import {
   Alert,
   Pressable,
@@ -7,40 +9,70 @@ import {
   TextInput,
   View,
 } from 'react-native';
+import {RootStackParamList} from '../../App';
 
-function SignIn() {
+type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
+function SignIn({navigation}: SignUpScreenProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const emailRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+
+  const onSignIn = useCallback(() => {
+    navigation.navigate('SignUp');
+  }, [navigation]);
+
   const onSubmit = useCallback(() => {
-    Alert.alert('알림창', '로그인 되었습니다.');
-  }, []);
+    if (!email || !email.trim()) {
+      Alert.alert('알림창', '이메일을 입력해주세요.');
+    }
+    if (!password || !password.trim()) {
+      Alert.alert('알림창', '비밀번호를 입력해주세요.');
+    }
+    if (email.trim() && password.trim()) {
+      Alert.alert('알림창', '로그인 되었습니다.');
+    }
+    console.log(email.trim());
+    console.log(!password.trim());
+  }, [email, password]);
 
   const onChangeEmail = useCallback((text: string) => {
-    setEmail(text);
+    setEmail(text.trim());
   }, []);
 
   const onChangePassword = useCallback((text: string) => {
-    setPassword(text);
+    setPassword(text.trim());
   }, []);
 
   const canGoNext = email && password;
 
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.inputZone}>
-        <Text style={styles.label}>이메일</Text>
+        <Text style={styles.label}>Email</Text>
         <TextInput
           style={styles.input}
           placeholder="이메일을 입력해주세요"
           onChangeText={onChangeEmail}
+          autoComplete="email"
+          textContentType="emailAddress"
+          returnKeyType="next"
+          onSubmitEditing={() => {
+            passwordRef.current?.focus();
+          }}
+          keyboardType="email-address"
+          blurOnSubmit={false}
+          ref={emailRef}
         />
       </View>
-      <View>
-        <Text style={styles.label}>비밀번호</Text>
+      <View style={styles.inputZone}>
+        <Text style={styles.label}>Password</Text>
         <TextInput
           style={styles.input}
           placeholder="비밀번호를 입력해주세요"
           onChangeText={onChangePassword}
+          secureTextEntry
+          ref={passwordRef}
         />
       </View>
       <View style={styles.buttonZone}>
@@ -52,10 +84,12 @@ function SignIn() {
               : StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
           }
           disabled={!canGoNext}>
-          <Text style={styles.loginButtonText}>로그인</Text>
+          <Text style={styles.ButtonText}>로그인</Text>
         </Pressable>
-        <Pressable onPress={onSubmit} style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>회원가입하기</Text>
+        <Pressable
+          onPress={onSignIn}
+          style={StyleSheet.compose(styles.loginButton, styles.signUpButton)}>
+          <Text style={styles.ButtonText}>회원가입하기</Text>
         </Pressable>
       </View>
     </View>
@@ -63,6 +97,13 @@ function SignIn() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
   loginButton: {
     backgroundColor: 'gray',
     paddingHorizontal: 20,
@@ -73,22 +114,35 @@ const styles = StyleSheet.create({
   loginButtonActive: {
     backgroundColor: 'blue',
   },
-  loginButtonText: {
+  ButtonText: {
     color: 'white',
   },
+  signUpButton: {
+    backgroundColor: 'red',
+  },
   buttonZone: {
+    margin: 10,
     alignItems: 'center',
   },
   inputZone: {
+    width: 340,
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   label: {
     fontSize: 20,
     fontWeight: 'bold',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
   },
   input: {
+    width: 200,
     backgroundColor: 'white',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    margin: 10,
   },
 });
 export default SignIn;
