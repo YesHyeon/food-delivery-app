3218404ec3a356e8ff57721ed0d7169773743c89
shgus1224 <shgus1224@naver.com>
2022-08-01 23:46:17 +0900

    주문 수락시 토큰 Refresh되도록

--- AppInner.tsx
+++ AppInner.tsx
@@ -128,7 +128,6 @@ function AppInner() {
       },
     );
   }, [dispatch]);
-
   // 앱 실행 시 토큰 있으면 로그인하는 코드
   useEffect(() => {
     const getTokenAndRefresh = async () => {
@@ -161,6 +160,38 @@ function AppInner() {
       }
     };
     getTokenAndRefresh();
+  }, [dispatch]);
+
+  useEffect(() => {
+    axios.interceptors.response.use(
+      response => {
+        return response;
+      },
+      async error => {
+        const {
+          config,
+          response: {status},
+        } = error;
+        if (status === 419) {
+          if (error.response.data.code === 'expired') {
+            const originalRequest = config;
+            const refreshToken = await EncryptedStorage.getItem('refreshToken');
+            // token refresh 요청
+            const {data} = await axios.post(
+              `${Config.API_URL}/refreshToken`, // token refresh api
+              {},
+              {headers: {authorization: `Bearer ${refreshToken}`}},
+            );
+            // 새로운 토큰 저장
+            dispatch(userSlice.actions.setAccessToken(data.data.accessToken));
+            originalRequest.headers.authorization = `Bearer ${data.data.accessToken}`;
+            // 419로 요청 실패했던 요청 새로운 토큰으로 재요청
+            return axios(originalRequest);
+          }
+        }
+        return Promise.reject(error);
+      },
+    );
   }, [dispatch]);
 
   return isLoggedIn ? (
--- src/components/EachOrder.tsx
+++ src/components/EachOrder.tsx
@@ -1,16 +1,52 @@
 import React, {useCallback, useState} from 'react';
-import {View, Text, StyleSheet, Button, Pressable} from 'react-native';
-import {Order} from '../slices/order';
+import {View, Text, StyleSheet, Button, Pressable, Alert} from 'react-native';
+import orderSlice, {Order} from '../slices/order';
+import {useAppDispatch} from '../store';
+import axios, {Axios, AxiosError} from 'axios';
+import Config from 'react-native-config';
+import {iteratorSymbol} from 'immer/dist/internal';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {LoggedInParamList} from '../../App';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 function EachOrder({item}: {item: Order}) {
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const navigation = useNavigation<NavigationProp<LoggedInParamList>>();
   const [detail, setDetail] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useAppDispatch();
 
   const onClick = () => {
     setDetail(prev => !prev);
   };
 
-  const onAccept = useCallback(() => {}, []);
-  const onReject = useCallback(() => {}, []);
+  const onAccept = useCallback(async () => {
+    if (!accessToken) {
+      return;
+    }
+    try {
+      await axios.post(
+        `${Config.API_URL}/accept`,
+        {orderId: item.orderId},
+        {headers: {authorization: `Bearer ${accessToken}`}},
+      );
+      dispatch(orderSlice.actions.acceptOrder(item.orderId));
+      navigation.navigate('Delivery');
+    } catch (error) {
+      let errorResponse = (error as AxiosError).response;
+      if (errorResponse?.status === 400) {
+        // 타인이 이미 수락한 경우
+        Alert.alert('알림', errorResponse.data.message);
+        dispatch(orderSlice.actions.rejectOrder(item.orderId));
+      }
+    }
+  }, [navigation, dispatch, item, accessToken]);
+
+  const onReject = useCallback(() => {
+    dispatch(orderSlice.actions.rejectOrder(item.orderId));
+  }, [dispatch, item]);
 
   return (
     <View key={item.orderId}>
@@ -18,15 +54,25 @@ function EachOrder({item}: {item: Order}) {
         <View style={styles.order}>
           <Text>닉네임 : {item.orderId}</Text>
           <Text>가격 : {item.price}원</Text>
+
           {detail ? (
-            <View>
-              <Pressable onPress={onAccept}>
-                <Text>수락</Text>
-              </Pressable>
-              <Pressable onPress={onReject}>
-                <Text>거절</Text>
-              </Pressable>
-            </View>
+            <>
+              <Text>네이버지도</Text>
+              <View style={styles.accept}>
+                <Pressable
+                  style={styles.onAccept}
+                  onPress={onAccept}
+                  disabled={loading}>
+                  <Text style={styles.acceptText}>수락</Text>
+                </Pressable>
+                <Pressable
+                  style={styles.onReject}
+                  onPress={onReject}
+                  disabled={loading}>
+                  <Text style={styles.acceptText}>거절</Text>
+                </Pressable>
+              </View>
+            </>
           ) : null}
         </View>
       </Pressable>
@@ -39,6 +85,34 @@ const styles = StyleSheet.create({
     backgroundColor: 'skyblue',
     margin: 6,
     padding: 5,
+  },
+  accept: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  onAccept: {
+    width: 80,
+    borderRadius: 10,
+    fontSize: 20,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5,
+  },
+  onReject: {
+    width: 80,
+    borderRadius: 10,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5,
+  },
+  acceptText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: 'white',
   },
 });
 export default EachOrder;
